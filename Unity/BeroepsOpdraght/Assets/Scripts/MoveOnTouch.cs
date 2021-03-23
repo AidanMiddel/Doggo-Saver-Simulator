4f6f9b5ab7b1d22480fe4dd00bc2b11478370ed4
yairme <yairstward2013@gmail.com>
2021-03-23 10:49:48 +0100

    Platforms & Scripts underdevelopment.

---
+++ Unity/BeroepsOpdraght/Assets/Scripts/MoveOnTouch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MoveOnTouch : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 velocity;
+
+    private bool moving;
+
+    private void OncollisionEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            moving = true;
+            collision.GetComponent<Collider>().transform.SetParent(transform);
+        }
+    }
+
+    private void OncollisionExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Collider>().transform.SetParent(null);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            transform.position += (velocity * Time.deltaTime);
+        }
+    }
+}
